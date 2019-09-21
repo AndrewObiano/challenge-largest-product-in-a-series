8f5fdd7011f469769797c1ba027c1744369f41a2
Andrew Obiano <andrew.obiano@gmail.com>
2019-09-21 10:01:15 -1000

    Finished

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,34 @@
 class LargestProduct {
   constructor(number) {
+    this.number = number;
+    this.index = 0;
+  }
 
+  getLargestProduct() {
+    let greatestProduct = 0;
+    let toArray = this.number.split("");
+    let digits = toArray.map(e => {
+      return parseInt(e);
+    });
+
+    for (let i = 0; i < digits.length; i++) {
+      let product =
+        digits[i] *
+        digits[i + 1] *
+        digits[i + 2] *
+        digits[i + 3] *
+        digits[i + 4];
+      if (product > greatestProduct) {
+        greatestProduct = product;
+        this.index = i;
+      }
+    }
+    return greatestProduct;
+  }
+
+  getLargestProductIndex() {
+    this.getLargestProduct();
+    return this.index;
   }
 }
 
